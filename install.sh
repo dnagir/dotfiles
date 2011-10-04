99c6eb18cb1c2d9491049d5f6cb948851afcaf06
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-04 13:09:24 +1100

    added ssh config and some aliases

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
 
 ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 echo ". ~/.bashrc # this file will be overwritten on next install" > ~/.bash_profile
+
+ln -sf ~/dotfiles/ssh/config ~/.ssh/config
