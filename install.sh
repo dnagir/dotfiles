a83e7feb5a1afb0504d632a0037ada568cf561e0
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-18 09:48:01 +1100

    enable vim mode for bash

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
 
 ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 echo ". ~/.bashrc # this file will be overwritten on next install" > ~/.bash_profile
+ln -sf ~/dotfiles/bash/intputrc ~/.inputrc
 
 ln -sf ~/dotfiles/ssh/config ~/.ssh/config
