13ea4628514368f4dff9a25dc1f14830b9580b2e
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-18 14:14:43 +1100

    added gitconfig

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 echo ". ~/.bashrc # this file will be overwritten on next install" > ~/.bash_profile
 ln -sf ~/dotfiles/bash/intputrc ~/.inputrc
 
+ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/ssh/config ~/.ssh/config
