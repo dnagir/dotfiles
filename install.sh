82b6538b1ffa45317ba5075b5dc5aa30a8fb4422
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-20 09:36:55 +1100

    add vim-ruby, update structure for vim

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!bash
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/vim/dotvim ~/.vim
 
 ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 echo ". ~/.bashrc # this file will be overwritten on next install" > ~/.bash_profile
