8c33df0f5cc4c81d80bb212429ee17495bcf9dd0
Dmytrii Nagirniak <dnagir@gmail.com>
2014-03-21 15:41:02 +1100

    Save current state

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln -sf ~/dotfiles/vim/dotvim ~/.vim
 
 ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 echo ". ~/.bashrc # this file will be overwritten on next install" > ~/.bash_profile
-ln -sf ~/dotfiles/bash/intputrc ~/.inputrc
+ln -sf ~/dotfiles/bash/inputrc ~/.inputrc
 
 ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/git/gitignore_global ~/.gitignore_global
