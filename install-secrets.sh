907e7a2228e6606b07c26dbb693c705e62604644
Dmytrii Nagirniak <dnagir@gmail.com>
2023-02-20 14:42:57 +1100

    Switch to Bitwarden
    Unfortunately this adds a dependency on `jq` as the `bw` CLI doesn't
    seem to have an option to extract the field values.

--- install-secrets.sh
+++ install-secrets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-echo >&2 "Installing SSH Keys from LastPass"
+echo >&2 "Installing SSH Keys from Bitwarden (remember to use bw unlock/login)!"
 mkdir -p $HOME/.ssh/
-lpass show 3028927856830506403 --field 'Private Key' > $HOME/.ssh/id_rsa
-lpass show 3028927856830506403 --field 'Public Key' > $HOME/.ssh/id_rsa.pub
+bw get item c7e1f659-efd5-4c14-8607-afaf003a3d52 | jq -r '.fields[] | select(.name=="PrivateKey") | .value' > $HOME/.ssh/id_rsa
+bw get item c7e1f659-efd5-4c14-8607-afaf003a3d52 | jq -r '.fields[] | select(.name=="PublicKey") | .value'  > $HOME/.ssh/id_rsa.pub
 
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
