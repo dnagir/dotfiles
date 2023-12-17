7084a9130c92ba5afc614806512125acc79acc8c
Dmytrii Nagirniak <dnagir@gmail.com>
2023-12-18 10:24:33 +1100

    nvim: Format init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,7 +43,7 @@ require("lazy").setup({
   },
   {
     "ray-x/go.nvim",
-    dependencies = {  -- optional packages
+    dependencies = { -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
@@ -51,8 +51,8 @@ require("lazy").setup({
     config = function()
       require("go").setup()
     end,
-    event = {"CmdlineEnter"},
-    ft = {"go", 'gomod'},
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
     --build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
 
