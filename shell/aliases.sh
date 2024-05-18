2cd049fab793dc270978dfe53c107a7462ff6e2a
Dmytrii Nagirniak <dnagir@gmail.com>
2024-05-18 21:08:59 +1000

    Add `awscurl` as an alias
    Shamefully I didn't know that the curl had built-in support for AWS SigV4
    (`--aws-sigv4`).
    
    It took a bit of an effort to craft a correct curl to invoke AWS API Gateway
    endpoints so a dedicated alias is useful.

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -26,3 +26,7 @@ alias docker-rmia='docker rmi $(docker images -q)'
 alias docker-stop-all='docker stop $(docker ps -q)'
 alias docker-clean-images="docker images -a > /tmp/docker-images-cleanup && vim /tmp/docker-images-cleanup && cat /tmp/docker-images-cleanup | docker rmi -f \$(awk 'NR!=1{print $3}')"
 alias docker-stats-short='docker stats --format "table {{.Name}} {{.Container}}\t{{.CPUPerc}}\t{{.MemPerc}}"'
+
+##### Misc
+
+alias awscurl='curl --aws-sigv4 "aws:amz:${AWS_REGION}:execute-api" --user "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" --header "x-amz-security-token:${AWS_SESSION_TOKEN}"'
