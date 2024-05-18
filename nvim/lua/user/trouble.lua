3b77138b87ae1f0337313edb903db845bc907f19
Dmytrii Nagirniak <dnagir@gmail.com>
2024-05-18 21:31:04 +1000

    nvim: Add `trouble` shortcuts

--- nvim/lua/user/trouble.lua
+++ nvim/lua/user/trouble.lua
@@ -2,3 +2,14 @@ local trouble = require("trouble")
 local tools = require("user.tools")
 
 tools.nmap("<leader>xx", function() trouble.toggle() end, { desc = "Trouble: toggle" })
+
+
+tools.nmap("<leader>xk", function()
+  trouble.open()
+  trouble.previous({ skip_groups = true, jump = true });
+end, { desc = "Trouble: next item" })
+
+tools.nmap("<leader>xj", function()
+  trouble.open()
+  trouble.next({ skip_groups = true, jump = true });
+end, { desc = "Trouble: next item" })
