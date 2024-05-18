dcc6b74973a7e61cfdbe78ed6b61bd9c1abeabee
Dmytrii Nagirniak <dnagir@gmail.com>
2024-05-18 21:12:09 +1000

    nvim: Update plugins

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -101,7 +101,9 @@ require("lazy").setup({
   {
     "nvim-neotest/neotest",
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
 
       "nvim-neotest/neotest-go",
